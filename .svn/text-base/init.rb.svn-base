85acfaa2a2f2149592c9801bac9db9e5063351fa
Ashabul Yeameen <yeameen@ubuntu.(none)>
2008-06-29 14:53:48 +0600

    first commit

---
+++ .svn/text-base/init.rb.svn-base
@@ -0,0 +1,4 @@
+#require File.join(File.dirname(__FILE__), "/lib/core_ext.rb")
+require File.join(File.dirname(__FILE__), "/lib/core_ext.rb")
+require File.join(File.dirname(__FILE__), "/lib/core_ext_hook.rb")
+
---
+++ lib/core_ext_hook.rb
@@ -0,0 +1,16 @@
+class ::Numeric
+  include GlobalizeNumeric::CoreExtensions::Numeric
+end
+
+class ::String
+  include GlobalizeNumeric::CoreExtensions::String
+end
+
+#class ::Locale
+#  include GlobalizeNumeric::Locale
+#end
+
+#Locale.class_eval do
+#  include GlobalizeNumeric::Locale
+#end
+
